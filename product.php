6178e2602993a040156f31c27fb28114338ec984
ZoyaLatif <124717752+ZoyaLatif@users.noreply.github.com>
2023-04-13 16:11:28 +0100

    Create product.php

---
+++ product.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<!--Your code goes here-->
+<?="The product of ${_GET['num1']} and ${_GET['num2']} is:"?>
+<h4><?=$_GET['num1']*$_GET['num2']?></h4>
+<a href="index.php">Reset</a>
+</body>
+</html>
