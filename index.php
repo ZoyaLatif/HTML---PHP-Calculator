f1d54d35efd66d15deb189f9c40ac73fda69df05
ZoyaLatif <124717752+ZoyaLatif@users.noreply.github.com>
2023-04-13 16:07:38 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,29 @@
+<html>
+<body>
+<h3>Addition</h3>
+<form action="addition.php" method="get">
+First Number: <input type="number" name="num1"><br>
+Second Number: <input type="number" name="num2"><br>
+<button type="submit">Add</button><br>
+</form>
+<h3>Subtraction</h3>
+<form action="subtraction.php" method="get">
+First Number: <input type="number" name="num1"><br>
+Second Number: <input type="number" name="num2"><br>
+<button type="submit">Minus</button><br>
+</form>
+<h3>Multiplication</h3>
+<form action="product.php" method="get">
+First Number: <input type="number" name="num1"><br>
+Second Number: <input type="number" name="num2"><br>
+<button type="submit">Product</button><br>
+</form>
+<h3>Division</h3>
+<form action="division.php " method="get">
+Numerator: <input type="number" name="n1"><br>
+Denominator: <input type="number" name="n2"><br>
+<button type="submit">Divide</button><br>
+</form>
+<a href="index.php">Reset</a>
+</body>
+</html>
