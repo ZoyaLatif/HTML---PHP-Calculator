9867a5f2bd1ebdebbb70f5b22e3279b885a823d3
ZoyaLatif <124717752+ZoyaLatif@users.noreply.github.com>
2023-04-13 16:08:26 +0100

    Create division.php

---
+++ division.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<!--Your code goes here-->
+<?="The division of ${_GET['n1']} and ${_GET['n2']} is:"?>
+<h4><?=$_GET['n1']/$_GET['n2']?></h4>
+<a href="index.php">Reset</a>
+</body>
+</html>
